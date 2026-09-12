603f94a0bc3899ad6884e0a1722f12a2b2e0b205
Yulia <yulia.condon@gmail.com>
2026-09-12 15:30:40 -0500

    created  headless orchard starter project

---
+++ Orchard_HeadLess_Starter/Program.cs
@@ -0,0 +1,13 @@
+using OrchardCore.Users.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+builder.Services.AddOrchardCms();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseOrchardCore();
+
+app.Run();
